62c8ac7a46bdb12184c9f66c004e161e2f2ee827
AnanyaN123 <76527702+AnanyaN123@users.noreply.github.com>
2021-02-04 11:12:58 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,13 @@
+class paper{
+    constructor(x, y, width, radius){
+        var options = {
+           isStatic : false,
+           restitution : 0.3,
+           friction : 0.5,
+           density : 1.2
+
+        }
+
+    }
+
+}
